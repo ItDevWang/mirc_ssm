a4201c435f251ebabd5503c94a8a9d7dc2f69882
ItDevWang <wangquanchn@163.com>
2019-09-15 21:51:27 +0800

    添加资源权限的查询所用和插入功能

--- mirc_ssm_dao/src/main/java/com/mirc/ssm/dao/IPermissionDao.java
+++ mirc_ssm_dao/src/main/java/com/mirc/ssm/dao/IPermissionDao.java
@@ -8,11 +8,27 @@ public interface IPermissionDao {
 
 
     /**
-     *
+     * 角色详情
      * @param id
      * @return
      * @throws Exception
      */
     List<Permission> findPermissionByRoleId(String id) throws Exception;
+
+    /**
+     * 查询所有
+     * @return
+     * @throws Exception
+     */
+    List<Permission> findAll() throws Exception;
+
+
+    /**
+     * 创建角色
+     * @throws Exception
+     */
+    void save(Permission permission) throws Exception;
+
+
 
 }
